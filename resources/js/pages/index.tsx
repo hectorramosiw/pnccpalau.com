851cf1e24d60bda0b1fa74c210c270eacbd557ce
Hector Ramos <hector.ramos@iconwireless.ph>
2025-08-06 13:57:08 +0800

    Add content and image to "Why PNCC?" section on home page

--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -114,9 +114,17 @@ export default function Index() {
             {/* [Why PNCC?] ::start */}
             <div className="w-screen bg-[#2babe2]">
                 <div className="container mx-auto">
-                    <p>
-                        test
-                    </p>
+                    <div className="flex flex-col md:flex-row items-center justify-items-center py-15">
+                        <div className="w-full md:w-[60%] p-4">
+                            <h2 className="text-3xl md:text-5xl font-bold text-white">Why PNCC?</h2>
+                            <p className="text-white mt-5">
+                                At PNCC, we've been keeping Palau connected since 1972. From mobile and internet to TV and custom tech solutions, we're here to serve every island and every community. Our team is passionate about bridging distances and bringing people closer - because for us, it's not just aboutthe technology, it's about connections.
+                            </p>
+                        </div>
+                        <div className="w-full md:w-[40%] p-4">
+                            <img src="/img/pages/index/img-06.png" alt="Why PNCC?" className="w-2/3 h-auto mx-auto" />
+                        </div>
+                    </div>
                 </div>
             </div>
             {/* [Why PNCC?] ::end */}
