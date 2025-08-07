661cc1f019d5ef7185c8395b93bf4ec5594e0ebd
Hector Ramos <hector.ramos@iconwireless.ph>
2025-08-07 17:40:15 +0800

    Enhance UI by adding background images to service cards and improving layout structure

--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -121,7 +121,7 @@ export default function Index() {
                     
                     <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-8">
                         <div className="flex flex-col items-center p-5">
-                            <div className="rounded-xl bg-gray-200/70 w-full h-full p-20 text-center">
+                            <div className="rounded-xl bg-gray-200/70 w-full h-full p-20 text-center relative before:content-[''] before:absolute before:top-0 before:right-0 before:w-[150px] before:h-[150px] before:bg-no-repeat before:bg-cover before:bg-right before:bg-[url('/img/ui/bakgrounds/img-01.png')] before:z-0">
                                 <img src="/img/ui/icons/cart.svg" alt="PNCC Palau Logo" className="h-auto w-16 mx-auto" />
                                 <p className="text-[#f96166] font-bold">
                                     Buy Prepaid Airtime or Internet Online
@@ -129,7 +129,7 @@ export default function Index() {
                             </div>
                         </div>
                         <div className="flex flex-col items-center p-5">
-                            <div className="rounded-xl bg-gray-200/70 w-full h-full p-20 text-center">
+                            <div className="rounded-xl bg-gray-200/70 w-full h-full p-20 text-center relative before:content-[''] before:absolute before:top-0 before:left-0 before:w-[100px] before:h-[100px] before:bg-no-repeat before:bg-cover before:bg-right before:bg-[url('/img/ui/bakgrounds/img-02.png')] before:z-0">
                                 <img src="/img/ui/icons/hand-cursor.svg" alt="PNCC Palau Logo" className="h-auto w-16 mx-auto" />
                                 <p className="text-[#f96166] font-bold">
                                     Pay my bill online, view bill history and check postpaid mobile data usage
@@ -137,7 +137,7 @@ export default function Index() {
                             </div>
                         </div>
                         <div className="flex flex-col items-center p-5">
-                            <div className="rounded-xl bg-gray-200/70 w-full h-full p-20 text-center">
+                            <div className="rounded-xl bg-gray-200/70 w-full h-full p-20 text-center relative before:content-[''] before:absolute before:top-0 before:right-0 before:w-[150px] before:h-[150px] before:bg-no-repeat before:bg-cover before:bg-right before:bg-[url('/img/ui/bakgrounds/img-03.png')] before:z-0">
                                 <img src="/img/ui/icons/hand-cursor.svg" alt="PNCC Palau Logo" className="h-auto w-16 mx-auto" />
                                 <p className="text-[#f96166] font-bold">
                                     Pay my bill online <br />
@@ -146,7 +146,7 @@ export default function Index() {
                             </div>
                         </div>
                         <div className="flex flex-col items-center p-5">
-                            <div className="rounded-xl bg-gray-200/70 w-full h-full p-20 text-center">
+                            <div className="rounded-xl bg-gray-200/70 w-full h-full p-20 text-center relative before:content-[''] before:absolute before:bottom-0 before:right-0 before:w-[120px] before:h-[120px] before:bg-no-repeat before:bg-cover before:bg-right before:bg-[url('/img/ui/bakgrounds/img-04.png')] before:z-0">
                                 <img src="/img/ui/icons/app.svg" alt="PNCC Palau Logo" className="h-auto w-16 mx-auto" />
                                 <p className="text-[#f96166] font-bold">
                                     Download the SmartHub App
@@ -154,7 +154,7 @@ export default function Index() {
                             </div>
                         </div>
                         <div className="flex flex-col items-center p-5">
-                            <div className="rounded-xl bg-gray-200/70 w-full h-full p-20 text-center">
+                            <div className="rounded-xl bg-gray-200/70 w-full h-full p-20 text-center relative before:content-[''] before:absolute before:top-0 before:right-0 before:w-[150px] before:h-[150px] before:bg-no-repeat before:bg-cover before:bg-right before:bg-[url('/img/ui/bakgrounds/img-05.png')] before:z-0">
                                 <img src="/img/ui/icons/swim-ring.svg" alt="PNCC Palau Logo" className="h-auto w-16 mx-auto" />
                                 <p className="text-[#f96166] font-bold">
                                     Customer Support: <br />
@@ -163,7 +163,7 @@ export default function Index() {
                             </div>
                         </div>
                         <div className="flex flex-col items-center p-5">
-                            <div className="rounded-xl bg-gray-200/70 w-full h-full p-20 text-center">
+                            <div className="rounded-xl bg-gray-200/70 w-full h-full p-20 text-center relative before:content-[''] before:absolute before:bottom-0 before:right-0 before:w-[120px] before:h-[120px] before:bg-no-repeat before:bg-cover before:bg-right before:bg-[url('/img/ui/bakgrounds/img-06.png')] before:z-0">
                                 <img src="/img/ui/icons/rewards.svg" alt="PNCC Palau Logo" className="h-auto w-16 mx-auto" />
                                 <p className="text-[#f96166] font-bold">
                                     Unlock exclusive rewards
@@ -176,14 +176,16 @@ export default function Index() {
             {/* [How can we help?] ::end */}
 
             {/* [Why PNCC?] ::start */}
-            <div className="w-screen bg-[#2babe2]">
-                <div className="container mx-auto">
-                    <div className="flex flex-col md:flex-row items-center justify-items-center py-15">
+            <div className="w-screen bg-[#2babe2] relative
+                before:content-[''] before:absolute before:top-0 before:right-0 before:w-[300px] before:h-[300px] before:bg-no-repeat before:bg-cover before:bg-right before:bg-[url('/img/ui/bakgrounds/shell-2.png')] before:z-0
+                after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-[300px] after:h-[300px] after:bg-no-repeat after:bg-cover after:bg-left after:bg-[url('/img/ui/bakgrounds/shell.png')] after:z-0"
+            >
+
+                <div className="container mx-auto py-15 relative z-10">
+                    <div className="flex flex-col md:flex-row items-center justify-items-center">
                         <div className="w-full md:w-[60%] p-4">
                             <h2 className="text-3xl md:text-5xl font-bold text-white">Why PNCC?</h2>
-                            <p className="text-white mt-5">
-                                At PNCC, we've been keeping Palau connected since 1972. From mobile and internet to TV and custom tech solutions, we're here to serve every island and every community. Our team is passionate about bridging distances and bringing people closer - because for us, it's not just aboutthe technology, it's about connections.
-                            </p>
+                            <p className="text-white mt-5"> At PNCC, we've been keeping Palau connected since 1972. From mobile and internet to TV and custom tech solutions, we're here to serve every island and every community. Our team is passionate about bridging distances and bringing people closer - because for us, it's not just aboutthe technology, it's about connections. </p>
                         </div>
                         <div className="w-full md:w-[40%] p-4">
                             <img src="/img/pages/index/img-06.png" alt="Why PNCC?" className="w-2/3 h-auto mx-auto" />
